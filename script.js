5dad6fad258f8ef91788edd9b516edc07f33f567
Israel Kab's <112182230+israelkabunangwa@users.noreply.github.com>
2025-05-13 12:16:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let bars=document.querySelector('.bars i')
+function openModal(){
+    
+    bars.addEventListener('click',()=>{
+        let modal=document.querySelector('.modal')
+        modal.style.display='block'
+        bars.style.display='none'
+    })
+    
+}
+openModal()
+function closeModal(){
+    let closeM=document.querySelector('.closeM')
+    let modal=document.querySelector('.modal')
+    closeM.addEventListener('click',()=>{
+        modal.style.display='none'
+        bars.style.display='block'
+    })
+
+    let listes=document.querySelectorAll('.modal ul li')
+    listes.forEach(list=>{
+        list.addEventListener('click',()=>{
+            modal.style.display='none'
+            bars.style.display='block'
+        })
+    })
+}
+closeModal();
